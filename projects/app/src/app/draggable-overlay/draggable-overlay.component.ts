ca93224c92b15ea78fa026556efd28f3cbe966b0
Tibing <nik.poltoratsky@gmail.com>
2020-09-18 21:44:23 +0300

    feat: draggable overlays

--- projects/app/src/app/draggable-overlay/draggable-overlay.component.ts
+++ projects/app/src/app/draggable-overlay/draggable-overlay.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
+import { Overlay, OverlayPositionBuilder, OverlayRef } from '@angular/cdk/overlay';
+import { TemplatePortal } from '@angular/cdk/portal';
 
 @Component({
   selector: 'app-draggable-overlay',
   templateUrl: './draggable-overlay.component.html',
   styleUrls: ['./draggable-overlay.component.css']
 })
-export class DraggableOverlayComponent implements OnInit {
+export class DraggableOverlayComponent {
 
-  constructor() { }
+  @ViewChild('dialog', { read: TemplateRef }) dialog: TemplateRef<any>;
 
-  ngOnInit(): void {
+  private ref: OverlayRef;
+
+  constructor(private overlay: Overlay,
+              private positionBuilder: OverlayPositionBuilder,
+              private vcr: ViewContainerRef) {
   }
 
+  show(): void {
+    this.ref = this.overlay.create({
+      hasBackdrop: true,
+      positionStrategy: this.positionBuilder.global()
+        .centerHorizontally()
+        .centerVertically(),
+    });
+
+    const userProfilePortal = new TemplatePortal(this.dialog, this.vcr);
+    this.ref.attach(userProfilePortal);
+  }
+
+  close(): void {
+    this.ref.dispose();
+  }
 }
